6637a8d4045c186baa9603ea7d54cd96f1875a2b
dharmikgorsiya <dharmik@convai.com>
2025-11-12 21:17:16 +0530

    Revert "Merge branch 'livekit:main' into main"
    This reverts commit e9d414abc0382bfdd73685b391771565da283745, reversing
    changes made to ca57fcf81c9d72f477f399e4085b21518e3b1dee.

--- Runtime/Scripts/Internal/FFIClients/FfiRequestExtensions.cs
+++ Runtime/Scripts/Internal/FFIClients/FfiRequestExtensions.cs
@@ -143,9 +143,6 @@ namespace LiveKit.Internal.FFIClients
                 case TextStreamWriterCloseRequest textStreamWriterCloseRequest:
                     ffiRequest.TextStreamClose = textStreamWriterCloseRequest;
                     break;
-                case SetRemoteTrackPublicationQualityRequest setRemoteTrackPublicationQualityRequest:
-                    ffiRequest.SetRemoteTrackPublicationQuality = setRemoteTrackPublicationQualityRequest;
-                    break;
                 default:
                     throw new Exception($"Unknown request type: {request?.GetType().FullName ?? "null"}");
             }
